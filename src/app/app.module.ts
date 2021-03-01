d522096d1f0841f256d418cc15ffe5c104fd37cd
beppe-dago <giuseppe.dago95@gmail.com>
2021-03-01 12:06:26 +0100

    scheletro app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListHeaderComponent } from './list-header/list-header.component';
+import { MainComponent } from './main/main.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ArticlesTipsComponent } from './articles-tips/articles-tips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListHeaderComponent,
+    MainComponent,
+    ArticleFormComponent,
+    ListComponent,
+    ListItemComponent,
+    ArticlesTipsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/articles-tips/articles-tips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-tips',
+  templateUrl: './articles-tips.component.html',
+  styleUrls: ['./articles-tips.component.css']
+})
+export class ArticlesTipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
